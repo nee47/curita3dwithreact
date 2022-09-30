9db7702ba9ed4f72285924729bbc652f0c1651b5
joseph <joseph_vv@hotmail.com>
2022-09-30 12:37:49 -0300

    gallery fixed

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Data from '../data.json'
 import { Link } from 'react-router-dom'
+import GalleryItem from '../components/GalleryItem'
 
 function Gallery() {
 
   return (
     <>   
-      <div className='main-container'>
+      <div className='gallery'>
+      <ul className='products'>
         {Data.map(item=>(
-          <div key={item.id}>
-            <Link to={`/productos/${item.id}`} alt='product'>
-              <img src={item.src} alt={item.name}/>
-            </Link>
-          </div>
+          <Link to={`/productos/${item.id}`} alt='product' key={item.id}>
+              {/* <img src={item.src} alt={item.name}/> */}
+            <GalleryItem        
+              src = {item.src}
+              alt = {item.name}
+              name = {item.name}
+              />
+          </Link>
         ))}
+      </ul>
       </div>
     </>
   )
