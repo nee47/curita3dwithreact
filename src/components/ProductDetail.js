0083b929e717882fcd9526d49f1a6e11af1eb188
joseph <joseph_vv@hotmail.com>
2022-09-30 23:54:45 -0300

    productos mejorados

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -9,11 +9,21 @@ function ProductDetail() {
 
    return (
     <>
-      <div className='productos'>
-        <h1>{founditem.name}</h1>
-        <div>{`el precio es ${founditem.price}`}</div>
-        <img src={founditem.src} alt={founditem.name} />
-        
+      <div className='main-detail'>
+        <div className='top-duo'>
+          <div className='top-duo__media'>
+            <img src= {founditem.src} alt= {founditem.name} />
+            <p>LAS IMAGENES SON AL EFECTO MERAMENTE ILUSTRATIVO Y NO SON CONTRACTUALES</p>
+          </div>
+          <div className='top-duo__name'>
+            <div className='top-duo__name__item-name'>{founditem.name}</div>
+            <div className='top-duo__name__item-price'>{`Precio $${founditem.price}`}</div>
+          </div>
+        </div>
+
+        <div className='description'>
+          <p>{founditem.description}</p>
+        </div>
       </div>
     </>
   )
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,26 +2,29 @@ import React from 'react'
 import Data from '../data.json'
 import { Link } from 'react-router-dom'
 import GalleryItem from '../components/GalleryItem'
+import Footer from '../components/Footer'
 
 function Gallery() {
 
   return (
     <>   
       <div className='gallery'>
-      <ul className='products'>
-        {Data.map(item=>(
-          <Link to={`/productos/${item.id}`} alt='product' key={item.id}>
-              {/* <img src={item.src} alt={item.name}/> */}
-            <GalleryItem        
-              src = {item.src}
-              alt = {item.name}
-              name = {item.name}
-              />
-          </Link>
-        ))}
-      </ul>
+        <ul className='products'>
+          {Data.map(item=>(
+            <Link to={`/productos/${item.id}`} alt='product' key={item.id}>
+                {/* <img src={item.src} alt={item.name}/> */}
+              <GalleryItem        
+                src = {item.src}
+                alt = {item.name}
+                name = {item.name}
+                />
+            </Link>
+          ))}
+        </ul>
       </div>
+      <Footer/>
     </>
+    
   )
 }
 
